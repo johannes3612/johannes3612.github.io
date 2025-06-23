611f6c3d9a61e22d4b3466665d82069c7e4940d1
johannes oosten <102830561+johannes3612@users.noreply.github.com>
2025-06-23 13:21:25 +0200

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,8 @@
+
+export const LocalStorageKeys = {
+  USERS: "familyTreeApp_users",
+  FAMILY_DATA: "familyTreeApp_familyData",
+};
+
+export const DEFAULT_USER_USERNAME = "erik";
+export const DEFAULT_USER_PASSWORD = "1234"; // In a real app, never hardcode passwords
